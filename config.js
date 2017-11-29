db90168a421e30db3145800614a812bf2e49095b
prakashbaskar <prakashbaskar@gmail.com>
2017-11-28 19:08:06 -0500

    Update styles, config and code cleanup

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {};
+
+config.twitterApiBaseURL = "https://api.twitter.com/1.1/search/tweets.json?";
+config.consumerKey = "tHwjzqVARCMJxNh24TniCm9vT";
+config.consumerSecret = "935322602314321921-sN57l6I7bjLjWWolGhQwLguaYPk6kaA";
+config.user1 = "realDonaldTrump";
+config.user2 = "BarackObama";
+module.exports = config;
--- server.js
+++ server.js
@@ -1,25 +1,27 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var cors = require('cors')
 var path = require('path')
 var request = require('request');
 var twitterFeed1 = require("./data/feed1.json");
 var twitterFeed2 = require("./data/feed2.json");
+var config = require('./config');
 
-app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Serve static files
 app.use(express.static(path.join(__dirname, 'style')));
 
+// Root path to load the view page
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/view/index.html');
 });
 
+// Api path to get twitter feed data
 app.get('/getTweets', function (req, res) {
-	var twitterApiUrl_1 = "https://api.twitter.com/1.1/search/tweets.json?oauth_consumer_key=tHwjzqVARCMJxNh24TniCm9vT&oauth_token=935322602314321921-sN57l6I7bjLjWWolGhQwLguaYPk6kaA&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1511843223&oauth_nonce=L0hyit&oauth_version=1.0&oauth_signature=byia7lvQ95G77kFiBDJGGVWvaOY%3D&q=realDonaldTrump&result_type=mixed&count=10";
-	var twitterApiUrl_2 = "https://api.twitter.com/1.1/search/tweets.json?oauth_consumer_key=tHwjzqVARCMJxNh24TniCm9vT&oauth_token=935322602314321921-sN57l6I7bjLjWWolGhQwLguaYPk6kaA&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1511843317&oauth_nonce=DrCH0y&oauth_version=1.0&oauth_signature=2X4aUaD25zD8YyYRGxwknz8qP4c%3D&q=BarackObama&result_type=mixed&count=10";
+	var twitterApiUrl_1 = `${config.twitterApiBaseURL}oauth_consumer_key=${config.consumerKey}&oauth_token=${config.consumerSecret}&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1511843223&oauth_nonce=L0hyit&oauth_version=1.0&oauth_signature=byia7lvQ95G77kFiBDJGGVWvaOY%3D&q=${config.user1}&result_type=mixed&count=10`;
+	var twitterApiUrl_2 = `${config.twitterApiBaseURL}oauth_consumer_key=${config.consumerKey}&oauth_token=${config.consumerSecret}&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1511843317&oauth_nonce=DrCH0y&oauth_version=1.0&oauth_signature=2X4aUaD25zD8YyYRGxwknz8qP4c%3D&q=${config.user2}&result_type=mixed&count=10`;
 	
 	// Request Feeds for 1st User
 	request(twitterApiUrl_1, function (error, response, body) {
@@ -32,7 +34,7 @@ app.get('/getTweets', function (req, res) {
 			console.log(`Unable to call Twitter Feed1 Error: ${error}`);
 		}
 		var tweets = twitterResponse1.statuses.map((item) => {
-			return {"id": item.id, "text": item.text, "created_at": item.created_at, "user": "DonaldTrump"};
+			return {"id": item.id, "text": item.text, "created_at": item.created_at, "user": config.user1};
 		})
 		
 		// Request Feeds for 2nd User
@@ -46,7 +48,7 @@ app.get('/getTweets', function (req, res) {
 					console.log(`Unable to call Twitter Feed2 Error: ${error2}`);
 				}
 				var tweets2 = twitterResponse2.statuses.map((item) => {
-					return {"id": item.id, "text": item.text, "created_at": item.created_at, "user": "BarackObama"};
+					return {"id": item.id, "text": item.text, "created_at": item.created_at, "user": config.user2};
 				})
 				tweets = tweets.concat(tweets2);
 				res.status(200).json(tweets);
@@ -54,6 +56,7 @@ app.get('/getTweets', function (req, res) {
 	})
 })
 
+// Configure and start the server in port 9000.
 var server = app.listen(9000, 'localhost', function () {
   var host = server.address().address
   var port = server.address().port
